19f728987b921f34ad4fbbcadf64aa765e61033a
Wenduo <jacky.ji12345@gmail.com>
2023-11-28 16:31:08 -0500

    New changes to image links and projects page

---
+++ src/components/homepage/skills.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+
+import Card from "../common/card";
+
+import "./styles/works.css";
+
+const Skills = () => {
+	return (
+		<div className="work">
+			<Card
+				icon={faBriefcase}
+				title="Skills"
+				body={
+					<div className="works-body">
+						<div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/python.png"}
+								alt="facebook"
+								className="work-image"
+							/>
+							<div className="work-title">Python</div>
+						</div>
+
+						<div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/java.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">Java</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/C.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">C Programming</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/C-sharp.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">C# Programming</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/html.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">HTML</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/css.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">CSS</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/javascript.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">JavaScript</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/sql.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">SQL</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/react.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">React</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/unity.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">Unity</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/git.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">Git</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/android-studio.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">Android Studio</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/nodejs.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">Node.js</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/mongodb.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">MongoDB</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/mysql.png"}
+								alt="twitter"
+								className="work-image"
+							/> 
+							<div className="work-title">MySQL</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src={process.env.PUBLIC_URL + "/icons/postgresql.png"}
+								alt="twitter"
+								className="work-image"
+							/>
+							<div className="work-title">PostGreSQL</div>
+						</div>
+					</div>
+				}
+			/>
+		</div>
+	);
+};
+
+export default Skills;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+                            Project Gallery
+						</div>
+
+						<div className="subtitle projects-subtitle">
+                            Dive into a collection of my computer science projects, 
+                            each reflecting a journey of exploration and learning. 
+                            From mobile games to web applications, these projects represent 
+                            my hands-on experience in diverse areas of software development. 
+                            If any of the projects catch your interest, I welcome you to 
+                            explore the code and share your thoughts on potential improvements or 
+                            enhancements. Collaborating with others is a wonderful opportunity for mutual 
+                            learning and growth, and I genuinely appreciate new ideas and feedback.
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
